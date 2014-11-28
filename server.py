70ba73dbebfc1f873524f7da7959f990715b7245
albagcs <albaszc@hotmail.com>
2014-11-28 23:38:57 +0100

    Update server.py
    Está todo bien, te he cambiado a que es un servidor SIP, en vez de echo UDP, te añadí lo que nos comentó Raquel, y al hacer eso te cambié el nombre del archivo a fich para que pasara el pep8. Te puse Ringing en vez de Ring, así nuestros UAS funcionan bien, lo dijo en el último momento que cambiáramos Ring por Ringing, que era una errata, pero no pasa nada.
    En el cliente estaba todo bien, te cambié Ring por Ringing también.
    Ya está :)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor de eco en UDP simple
+Clase (y programa principal) para un servidor SIP
 """
 
 import SocketServer
@@ -17,6 +17,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
+            IP_CLIENT = str(self.client_address[0])
             # Leyendo línea a línea lo que nos envía el cliente
             #SIP/2.0 200 OK\r\n\r\n
             line = self.rfile.read()
@@ -26,13 +27,13 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             # Si no hay más líneas salimos del bucle infinito
             if line[0] == "INVITE":
                 print "INVITE recibido"
-                self.wfile.write("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring"
+                self.wfile.write("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ringing"
                                  "\r\n\r\nSIP/2.0 200 OK\r\n\r\n")
 
             elif line[0] == "ACK":
                 print "ACK recibido"
             # aEjecutar es un string con lo que se ha de ejecutar en la shell
-                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + fich_audio
+                aEjecutar = './mp32rtp -i' + IP_CLIENT + '-p 23032 < ' + fich
                 print "Vamos a ejecutar", aEjecutar
                 os.system('chmod 755 mp32rtp')
                 os.system(aEjecutar)
@@ -43,7 +44,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             elif (line[0] == "CANCEL" or line[0] == "REGISTER"
                   or line[0] == "OPTIONS"):
                 print "metodo no disponible recibido"
-                self.wfile.write("SIP/2.0 Method Not Allowed\r\n\r\n")
+                self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
             else:
                 print "petición incorrecta recibida"
                 self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
@@ -54,8 +55,7 @@ if __name__ == "__main__":
     else:
         print "Listening..."
     # Creamos servidor de eco y escuchamos
-    fich_audio = sys.argv[3]
+    fich = sys.argv[3]
     PORT = int(sys.argv[2])
     serv = SocketServer.UDPServer(("", PORT), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
